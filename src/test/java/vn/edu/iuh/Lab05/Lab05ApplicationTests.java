64c0647cb00eb8ba6e8f4006337a343c9b12659c
Dawyy I <21045081.y@student.iuh.edu.vn>
2024-12-02 09:55:47 +0700

    create project

---
+++ src/test/java/vn/edu/iuh/Lab05/Lab05ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.Lab05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
