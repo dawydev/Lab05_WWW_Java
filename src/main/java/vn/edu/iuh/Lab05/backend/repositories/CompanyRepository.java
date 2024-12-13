34fb2730cfd9584aa76d260fa230a0538dc28c56
Dawyy I <21045081.y@student.iuh.edu.vn>
2024-12-14 02:43:19 +0700

    adding job skill

--- src/main/java/vn/edu/iuh/Lab05/backend/repositories/CompanyRepository.java
+++ src/main/java/vn/edu/iuh/Lab05/backend/repositories/CompanyRepository.java
@@ -1,8 +1,9 @@
 package vn.edu.iuh.Lab05.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import vn.edu.iuh.Lab05.backend.models.Account;
 import vn.edu.iuh.Lab05.backend.models.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Company findByEmail(String email);
+    Company findByAccount(Account account);
 }
--- src/main/java/vn/edu/iuh/Lab05/frontend/controllers/CompanyController.java
+++ src/main/java/vn/edu/iuh/Lab05/frontend/controllers/CompanyController.java
@@ -1,17 +1,17 @@
 package vn.edu.iuh.Lab05.frontend.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import vn.edu.iuh.Lab05.backend.models.Account;
 import vn.edu.iuh.Lab05.backend.models.Company;
 import vn.edu.iuh.Lab05.backend.models.Job;
 import vn.edu.iuh.Lab05.backend.repositories.CompanyRepository;
 import vn.edu.iuh.Lab05.backend.repositories.JobRepository;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 public class CompanyController {
@@ -23,11 +23,15 @@ public class CompanyController {
     private JobRepository jobRepository;
 
     @GetMapping("/company/dashboard")
-    public String showDashboard(Model model) {
-        List<Company> companies = companyRepository.findAll();
-        Map<Company, List<Job>> companyJobsMap = companies.stream()
-                .collect(Collectors.toMap(company -> company, company -> jobRepository.findByCompany(company)));
-        model.addAttribute("companyJobsMap", companyJobsMap);
-        return "company/dashboard";
+    public String showDashboard(Model model, HttpSession session) {
+        Account loggedInUser = (Account) session.getAttribute("loggedInUser");
+        if (loggedInUser != null && "COMPANY".equals(loggedInUser.getRole())) {
+            Company company = companyRepository.findByAccount(loggedInUser);
+            List<Job> jobs = jobRepository.findByCompany(company);
+            model.addAttribute("company", company);
+            model.addAttribute("jobs", jobs);
+            return "company/dashboard";
+        }
+        return "redirect:/login";
     }
 }
